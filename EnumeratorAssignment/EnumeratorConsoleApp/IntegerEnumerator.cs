d9edb01866d73a78e65274ca6087af782f0a6147
leontin.murani <leontin.murani@gmail.com>
2020-06-19 14:13:04 +0300

    Commit assignment

---
+++ EnumeratorAssignment/EnumeratorConsoleApp/IntegerEnumerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnumeratorConsoleApp
+{
+    public static class IntegerEnumerator
+    {
+        public static IEnumerable<int> GenerateAllIntegers(int lastValue = int.MaxValue)
+        {
+            if (lastValue < 0)
+            {
+                throw new IndexOutOfRangeException("The upper bound of the list is lower than 0");
+            }
+
+            if (lastValue > int.MaxValue)
+            {
+                throw new IndexOutOfRangeException("The upper bound of the list is higher than the maximum value for Int32");
+            }
+
+            for (int i = 0; i <= lastValue; i++)
+            {
+                yield return i;
+            }
+        }
+
+        public static IEnumerable<int> GenerateAllOddIntegers(IEnumerable<int> input)
+        {
+            foreach (var item in input)
+            {
+                if (item % 2 is 1)
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
---
+++ EnumeratorAssignment/EnumeratorConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EnumeratorConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //foreach (int item in IntegerEnumerator.GenerateAllIntegers(265))
+            //{
+            //    Console.WriteLine(item);
+
+            //}
+
+            foreach (var item in IntegerEnumerator.GenerateAllOddIntegers(IntegerEnumerator.GenerateAllIntegers(38)))
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}

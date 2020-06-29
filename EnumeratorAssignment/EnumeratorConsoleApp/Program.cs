b07948c28439a7ceea9f52bc7e5964f0973031a1
leontin.murani <leontin.murani@gmail.com>
2020-06-29 12:23:30 +0300

    Added a LINQ method to display the same results from the second requirement.

--- EnumeratorAssignment/EnumeratorConsoleApp/Program.cs
+++ EnumeratorAssignment/EnumeratorConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.Linq;
 
 namespace EnumeratorConsoleApp
 {
@@ -12,11 +14,19 @@ namespace EnumeratorConsoleApp
 
             //}
 
+            Console.WriteLine("Results displayed using Delegates:");
             foreach (var item in IntegerEnumerator.GenerateAllOddIntegers(IntegerEnumerator.GenerateAllIntegers(38)))
             {
-                Console.WriteLine(item);
+                Console.WriteLine(item.ToString(), Console.ForegroundColor = ConsoleColor.Green);
             }
 
+            Console.ResetColor();
+
+            //using LINQ
+            Console.WriteLine();
+            Console.WriteLine("Results displayed using LINQ:");
+            IntegerEnumerator.GenerateAllIntegers(38).Where(number => number % 2 == 1)
+                .ToList().ForEach(number => Console.WriteLine(number.ToString(), Console.ForegroundColor = ConsoleColor.Red));
 
             Console.ReadLine();
         }
